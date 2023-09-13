844528100fdef55c67a1c7075bbf5dd7374ffbfb
Marttins <89866234+Marttins011@users.noreply.github.com>
2023-09-13 18:18:14 +0100

    Changed the way it gets the ped gender

--- client.lua
+++ client.lua
@@ -1,30 +1,37 @@
-local Core = exports[Config.Core]:GetCoreObject()
-local DefaultHair = nil
-local DefaultHairTexture = nil
+local defaultHair = nil
+local defaultHairTexture = nil
 
 function playAnim(animDict, animName, duration, flag)
 	lib.requestAnimDict(animDict, 100)
     TaskPlayAnim(PlayerPedId(), animDict, animName, 1.0, -1.0, duration, flag, 1, false, false, false)
 end
 
+function getPedSex()
+    local model = GetEntityModel(PlayerPedId())
+    if model == GetHashKey("mp_m_freemode_01") then
+        return "male"
+    elseif model == GetHashKey("mp_f_freemode_01") then
+        return "female"
+    end
+end
+
 RegisterNetEvent('mt-scrunchie:client:useScrunchie', function()
-    local PlayerData = Core.Functions.GetPlayerData()
-    local gender = PlayerData.charinfo.gender
+    local gender = getPedSex()
     local ped = PlayerPedId()
     
     playAnim(Config.animDict, Config.animName, 1000)
     Wait(1000)
-    if not DefaultHair or not DefaultHairTexture then
-        DefaultHair = GetPedDrawableVariation(ped, 2)
-        DefaultHairTexture = GetPedTextureVariation(ped, 2)
-        if gender == 1 then
-            SetPedComponentVariation(ped, 2, Config.femaleHair, DefaultHairTexture, 2)
-        else
-            SetPedComponentVariation(ped, 2, Config.maleHair, DefaultHairTexture, 2)
+    if not defaultHair or not defaultHairTexture then
+        defaultHair = GetPedDrawableVariation(ped, 2)
+        defaultHairTexture = GetPedTextureVariation(ped, 2)
+        if gender == 'male' then
+            SetPedComponentVariation(ped, 2, Config.femaleHair, defaultHairTexture, 2)
+        elseif gender == 'female' then
+            SetPedComponentVariation(ped, 2, Config.maleHair, defaultHairTexture, 2)
         end
     else
-        SetPedComponentVariation(ped, 2, DefaultHair, DefaultHairTexture, 2)
-        DefaultHair = nil
-        DefaultHairTexture = nil
+        SetPedComponentVariation(ped, 2, defaultHair, defaultHairTexture, 2)
+        defaultHair = nil
+        defaultHairTexture = nil
     end
 end)
